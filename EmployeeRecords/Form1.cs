d18a16882b0295d2de232b244ec867351ae712fc
Malcolm <wrightmbw01@gmail.com>
2019-04-04 09:47:36 -0400

    Finished reading and writing to xml

--- EmployeeRecords/Form1.cs
+++ EmployeeRecords/Form1.cs
@@ -17,21 +17,52 @@ namespace EmployeeRecords
         public mainForm()
         {
             InitializeComponent();
+            loadDB();
         }
 
         private void addButton_Click(object sender, EventArgs e)
-        { 
+        {
+            Employee emp = new Employee(idInput.Text,
+                fnInput.Text, 
+                lnInput.Text, 
+                dateInput.Text, 
+                salaryInput.Text);
+
+            employeeDB.Add(emp);
+
+            outputLabel.Text = "new employee added";
+
             ClearLabels();
         }
 
         private void removeButton_Click(object sender, EventArgs e)
         {
-           
+           foreach (Employee emp in employeeDB)
+            {
+                if (emp.id == idInput.Text)
+                {
+                    outputLabel.Text = "employee removed";
+                    employeeDB.Remove(emp);
+                    ClearLabels();
+                    return;
+                }
+            }
+
+            outputLabel.Text = "employee not found";
+            ClearLabels();
         }
 
         private void listButton_Click(object sender, EventArgs e)
         {
+            outputLabel.Text = "";
 
+            foreach (Employee emp in employeeDB)
+            {
+                outputLabel.Text += emp.id + " " + emp.firstName
+                    + " " + emp.lastName
+                    + " " + emp.date
+                    + " " + emp.salary + "\n";
+            }
         }
 
         private void ClearLabels()
@@ -45,17 +76,64 @@ namespace EmployeeRecords
 
         private void mainForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-
+            saveDB();
         }
 
         public void loadDB()
         {
-            
+            string newId, newFN, newLN, newDate, newSalary;
+
+            XmlReader reader = XmlReader.Create("Resources/EmployeeData.xml");
+
+            // while there is another chunk to read
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Text)
+                {
+                    newId = reader.ReadString();
+
+                    reader.ReadToNextSibling("firstName");
+                    newFN = reader.ReadString();
+
+                    reader.ReadToNextSibling("lastName");
+                    newLN = reader.ReadString();
+
+                    reader.ReadToNextSibling("startDate");
+                    newDate = reader.ReadString();
+
+                    reader.ReadToNextSibling("salary");
+                    newSalary = reader.ReadString();
+
+                    Employee emp = new Employee(newId, newFN, newLN, newDate, newSalary);
+                    employeeDB.Add(emp);
+                }
+            }
+
+            reader.Close();
         }
 
         public void saveDB()
         {
+            XmlWriter writer = XmlWriter.Create("Resources/EmployeeData.xml", null);
 
+            writer.WriteStartElement("Employees");
+
+            foreach (Employee emp in employeeDB)
+            {
+                writer.WriteStartElement("Employee");
+
+                writer.WriteElementString("id", emp.id);
+                writer.WriteElementString("firstName", emp.firstName);
+                writer.WriteElementString("lastName", emp.lastName);
+                writer.WriteElementString("startDate", emp.date);
+                writer.WriteElementString("salary", emp.salary);
+
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndElement();
+
+            writer.Close();
         }
     }
 }
